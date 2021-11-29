ced4d1f1bd52fede067bcf00070acf3ca89a9e3b
salvarez <salvarez@voxelgroup.net>
2021-11-29 13:28:40 +0100

    refactor: generalize a solution for multiples of 3

--- FizzBuzzKata2/FizzBuzzCalculator.cs
+++ FizzBuzzKata2/FizzBuzzCalculator.cs
@@ -4,17 +4,7 @@ namespace FizzBuzzKata2
     {
         public static string Print(int i)
         {
-            if (i == 3)
-            {
-                return "Fizz";
-            }
-
-            if (i == 6)
-            {
-                return "Fizz";
-            }
-
-            if (i == 9)
+            if (i % 3 == 0)
             {
                 return "Fizz";
             }
